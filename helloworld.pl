d91c7ea21267f809269a8233b438de3ec927b73b
ltang <tony.tanglin@163.com>
2015-01-09 16:14:21 +0800

    helloworld.pl
    first perl

---
+++ helloworld.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+
+#printf "hello world!\n";
+#printf 'hello world!\n';
+
+#$a = 10;
+#printf "value of a= $a\n";
+#printf 'value of a= $a\n';
+
+
+#$a="fred" x 3;
+#$b=5x4.8;
+#printf $a;
+#printf "$b \n";
+
+#printf "6*7 \n";
+#printf 6*7;
+
+#printf "the answer is",7*8,".\n";
+
+$what = "bron steak";
+$n = 3;
+printf "fred eat $n $whats.\n";
+printf "fred eat $n ${what}s.\n";
+printf "fred eat $n $what" . "s.\n";
+printf 'fred eat ' . $n . ' ' . $what."s.\n";
